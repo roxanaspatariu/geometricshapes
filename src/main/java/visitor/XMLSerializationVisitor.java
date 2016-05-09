70ec5ad58b0b2c720660c8f3650e2c49ca0bdb8a
roxanaspatariu <roxanaspatariu15@gmail.com>
2016-05-09 08:48:56 +0300

    finished XML Creator

--- src/main/java/visitor/XMLSerializationVisitor.java
+++ src/main/java/visitor/XMLSerializationVisitor.java
@@ -9,42 +9,42 @@ import twodimensionalforms.*;
  */
 public class XMLSerializationVisitor implements PicturePartVisitor {
 
-    CreateXMLDocument createXMLDocument = new CreateXMLDocument();
+    CreateXMLDocument createXMLDocument = CreateXMLDocument.getInstance();
 
 
     @Override
     public void visit(Picture picture) {
-        createXMLDocument.setElementValue(picture.getName(), picture.getArea(), picture.getDimension(), picture.getFont().getColor(), picture.getFont().getBorderSize());
+        createXMLDocument.setElementValue(picture);
     }
 
     @Override
     public void visit(Circle circle) {
-        createXMLDocument.setElementValue(circle.getName(), circle.getArea(), circle.getDimension(), circle.getFont().getColor(), circle.getFont().getBorderSize());
+        createXMLDocument.setElementValue(circle);
     }
 
     @Override
     public void visit(Square square) {
-        createXMLDocument.setElementValue(square.getName(), square.getArea(), square.getDimension(), square.getFont().getColor(), square.getFont().getBorderSize());
+        createXMLDocument.setElementValue(square);
     }
 
     @Override
     public void visit(Trapeze trapeze) {
-        createXMLDocument.setElementValue(trapeze.getName(), trapeze.getArea(), trapeze.getDimension(), trapeze.getFont().getColor(), trapeze.getFont().getBorderSize());
-
+        createXMLDocument.setElementValue(trapeze);
     }
 
     @Override
     public void visit(Triangle triangle) {
-        createXMLDocument.setElementValue(triangle.getName(), triangle.getArea(), triangle.getDimension(), triangle.getFont().getColor(), triangle.getFont().getBorderSize());
+        createXMLDocument.setElementValue(triangle);
     }
 
     @Override
     public void visit(Cube cube) {
-        createXMLDocument.setElementValue(cube.getName(), cube.getArea(), cube.getDimension(), cube.getFont().getColor(), cube.getFont().getBorderSize());
+        createXMLDocument.setElementValue(cube);
     }
 
     @Override
     public void visit(Rectangle rectangle) {
-        createXMLDocument.setElementValue(rectangle.getName(), rectangle.getArea(), rectangle.getDimension(), rectangle.getFont().getColor(), rectangle.getFont().getBorderSize());
+        createXMLDocument.setElementValue(rectangle);
     }
+
 }
